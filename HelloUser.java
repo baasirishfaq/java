853dd95844d9e2ca53ba9e631e49a1e2d98f91b9
Baasir Ishfaq <baasirishfaq@gmail.com>
2025-04-29 02:52:51 -0400

    reading user input

--- HelloUser.java
+++ HelloUser.java
@@ -2,11 +2,23 @@ import java.util.Scanner;
 
 public class HelloUser {
     public static void main(String[] args) {
-        boolean isStudent = true;
-        if (!isStudent) {
-            System.out.println("Yes he is the student");
-        } else {
-            System.out.println("NO he is not a student");
-        }
+        Scanner nameMeAnything = new Scanner(System.in); // to declare that i am seeking keyboard input from the user
+        
+        System.out.print("enter your name: "); 
+        // simple message asking user what to input
+        // println is not being used because println leaves a newline.
+
+        String name = nameMeAnything.nextLine();
+        /*
+         * scanner catches the user input  
+         * a string called name is used to store that value which the user gave so we can reuse the
+         * value when ever we want
+         * using nextLine() spaces can also be read, it can read a full name example "John Doe".
+         * use next() if spaces are not needed to be read.
+         */
+
+        System.out.println("Hello " + name + " welcome on board"); // simple print line and the user's value is being
+                                                                   // used here
+        nameMeAnything.close(); // close the scanner tag
     }
 }
