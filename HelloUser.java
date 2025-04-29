1b9f07df9341b58194a1f280d904556151772fd2
Baasir Ishfaq <baasirishfaq@gmail.com>
2025-04-29 02:11:35 -0400

    if else boolean

---
+++ HelloUser.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HelloUser {
+    public static void main(String[] args) {
+        boolean isStudent = true;
+        if (!isStudent) {
+            System.out.println("Yes he is the student");
+        } else {
+            System.out.println("NO he is not a student");
+        }
+    }
+}
