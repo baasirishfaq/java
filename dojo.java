492d8d72a6204a920520f0223879fdab0dda0024
Baasir Ishfaq <baasirishfaq@gmail.com>
2025-04-30 01:36:52 -0400

    calculator using switch case

--- dojo.java
+++ dojo.java
@@ -2,20 +2,31 @@ import java.util.Scanner;
 
 public class dojo {
 
-    public static void main(String[] args)
-    {
-        // make an array of size 10
-        int[] arr = new int[10];
-        Scanner sc = new Scanner(System.in);
-        for(int i = 0; i<10; i++)
-        {
-            System.out.print("enter the number " + (i+1) + ": ");
-            arr[i] = sc.nextInt();
-        }
+    public static void main(String args[]) {
 
-        for(int it: arr)
-        {
-            System.out.print(it + " ");
+        float num1, num2;
+        System.out.print("Enter the first number: ");
+        Scanner sc = new Scanner(System.in);
+        num1 = sc.nextFloat();
+        System.out.print("Enter the second number: ");
+        num2 = sc.nextFloat();
+
+        System.out.print("Enter the symbol of operation you want on these numbers: + - * / :");
+        char op = sc.next().charAt(0);
+
+        switch (op) {
+            case '+':
+                System.out.println("The sum of the two numbers is: " + (num1 + num2));
+                break;
+            case '-':
+                System.err.println("The difference of the two numbers isL " + (num1 - num2));
+                break;
+            case '*':
+                System.err.println("The multiplication of the two numbers is : " + (num1 * num2));
+                break;
+            case '/':
+                System.err.print("The division of the two numbers is :" + (num1 / num2));
+                break;
         }
         sc.close();
     }
