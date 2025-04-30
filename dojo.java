96242ddfb2a94a7317bd3c6e1beb3ce053f77e27
Baasir Ishfaq <baasirishfaq@gmail.com>
2025-04-30 02:04:46 -0400

    guess calculator

--- dojo.java
+++ dojo.java
@@ -4,23 +4,23 @@ public class dojo {
 
     public static void main(String[] args) {
         int password = 1234;
-        int guessLeft = 3;
+        int attempts = 3;
         Scanner pass = new Scanner(System.in);
 
-        for(int i = 0; i < 3; i++) {
+        for (int i = 0; i < attempts; i++) {
             System.out.print("Enter your password: ");
-            int guess = pass.nextInt();
-
-            if (guess == password) {
-                System.out.println("Welcome Access Granted!");
+            int input = pass.nextInt();
+            if (input == password) {
+                System.out.println("Welcome!");
                 break;
             } else {
-                guessLeft--;
-                System.out.println("Wrong password! You have " + guessLeft + " guesses left.");
+                System.out.println("incorrect password : ");
+                System.out.println("you have " + (attempts - (i + 1)) + " tries left");
             }
-            if (guessLeft == 0) {
+            if (i == 2) {
                 System.out.println("You have been locked out.");
             }
         }
+        pass.close();
     }
 }
