f6872c173bbcc3207aa985ca5926ebaeb18ce40b
Baasir Ishfaq <baasirishfaq@gmail.com>
2025-05-01 02:11:48 -0400

    minor updates

--- dojo.java
+++ dojo.java
@@ -17,26 +17,28 @@ public class dojo {
 
         while (guess != number) {
             if (attempts == allowed) {
-                System.out.println("you have reached the maximum number of attempts you lose");
+                System.out.println("you have reached the maximum number of attempts you lose" +" the number was "+ number);
                 break;
             }
-            if (Math.abs(guess - number) <= 5) {
-                System.out.println("you are very close");
+            if (Math.abs(guess - number) <= 2) {
+                System.out.println("Almost there");
+            } else if (Math.abs(number - guess) <= 5) {
+                System.out.println("Even closer");
             } else if (Math.abs(guess - number) <= 10) {
                 System.out.println("You're close but not close enough");
             } else if (Math.abs(guess - number) <= 20) {
                 System.out.println("getting closer");
             } else if (guess > number) {
-                System.out.println("too high not even close");
+                System.out.println("too high ");
             } else if (guess < number) {
-                System.out.println("too low not even close");
+                System.out.println("too low ");
             }
             System.out.print("try again: ");
             guess = input.nextInt();
             attempts++;
         }
         if (guess == number) {
-            System.out.println("correct! it took you " + attempts + " attempts");
+            System.out.println("correct! it took you " + attempts + " attempts " + "the number was "+ number);
         }
         input.close();
     }
