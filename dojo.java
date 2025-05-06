019c8f783f6b3a02fe32773cf2a905bf21d42bb8
Baasir Ishfaq <baasirishfaq@gmail.com>
2025-05-05 23:34:56 -0400

    hackerrank prack

--- dojo.java
+++ dojo.java
@@ -1,21 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class dojo {
-
-    public static void main(String[] args) {
+class Solution {
+    public static void main(String []argh) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("================================");
+        int t = sc.nextInt();
 
-        for (int i = 0; i < 3; i++) {
-            String language = sc.next(); // e.g. "java"
-            int score = sc.nextInt();    // e.g. 100
-
-            // Format string: %-15s means left-align string in 15 characters
-            // %03d means pad the number with leading 0s to make 3 digits
-            System.out.printf("%-15s%03d%n", language, score);
+        for(int i = 0; i < t; i++) {
+            try {
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+                if(x >= -128 && x <= 127) System.out.println("* byte");
+                if(x >= -32768 && x <= 32767) System.out.println("* short");
+                if(x >= -2147483648 && x <= 2147483647) System.out.println("* int");
+                if(x >= -9223372036854775808L && x <= 9223372036854775807L) System.out.println("* long");
+            } catch(Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
         }
-
-        System.out.println("================================");
-        sc.close();
     }
 }
