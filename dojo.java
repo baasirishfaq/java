54af9308191b7501b91639ddcf74d50223e0e4c0
Baasir Ishfaq <baasirishfaq@gmail.com>
2025-05-09 02:22:07 -0400

    HACKERRANK SUBSTRING COMPARE

--- dojo.java
+++ dojo.java
@@ -1,20 +1,31 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Solution {
+        public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);
+        String compare;
+        
+        for (int i = 0; i <= s.length() - k; i++) {  
+            
+            compare = s.substring(i, i + k);
+            
+            if (compare.compareTo(smallest) < 0) {
+                smallest = compare;
+            }
+            if (compare.compareTo(largest) > 0) {
+                largest = compare;
+            }
+        }
+        
+        return smallest + "\n" + largest;
+    }
 
     public static void main(String[] args) {
-        
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        
-        String reversedStr = "";
-
-        for (int i = 0; i < A.length(); i++) 
-        {
-            reversedStr = A.charAt(i) + reversedStr;
-        }
-        System.out.println(reversedStr.equals(A) ? "Yes" : "No");
-        sc.close();
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
     }
-}
