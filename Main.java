ecceee01f52a7b57a3664bcf5136b0e7e4383ecc
Baasir Ishfaq <baasirishfaq@gmail.com>
2025-12-07 22:25:46 -0500

    e

--- Main.java
+++ Main.java
@@ -1,39 +1,28 @@
-import java.util.Scanner;
-import java.util.Random;
+class Car {
+    Car() {
+        System.out.println("Car constructor");
+    }
+
+    void engine() {
+        System.out.println("V6 engine");
+    }
+}
+
+class Tesla extends Car {
+    Tesla() {
+        System.out.println("Tesla constructor");
+    }
+
+    @Override
+    void engine() {
+        super.engine();
+        System.out.println("Electric engine");
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        Random rand = new Random();
-        int number = rand.nextInt(100) + 1;
-
-        System.out.println("the number is " + number);
-
-        System.out.print("enter your guess: ");
-        Scanner input = new Scanner(System.in);
-        int guess = input.nextInt();
-        int attempts = 1;
-        int maxAttempts = 10;
-        while (guess != number) {
-            if (Math.abs(guess - number) <= 10) {
-                System.out.println("close");
-            } else if (guess < number) {
-                System.out.println("too low");
-
-            } else {
-                System.out.println("too high");
-            }
-            if (attempts == maxAttempts) {
-                System.out.println("you have reached the maximum number of attempts");
-                break;
-            }
-            System.out.print("try again: ");
-            guess = input.nextInt();
-            attempts++;
-        }
-        if (guess == number)
-
-        {
-            System.out.println("correct! it took you " + attempts + " attempts");
-        }
+        Tesla t = new Tesla();
+        t.engine();
     }
 }
